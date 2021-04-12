fb6918784ea5e1393db4d9edb6d6052356e199cf
Giovani Godoy <43485102+giovaniGdy@users.noreply.github>
2021-04-12 14:14:55 -0300

    Arquivos das Aulas

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -45,5 +45,134 @@ function parDeBarreira(altura, abertura, x) {
   this.setX(x);
 }
 
-const b = new parDeBarreira(700, 200, 400);
-document.querySelector("[wm-flappy]").appendChild(b.elemento);
+// const b = new parDeBarreira(700, 200, 400);
+// document.querySelector("[wm-flappy]").appendChild(b.elemento);
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+  this.pares = [
+    new parDeBarreira(altura, abertura, largura),
+    new parDeBarreira(altura, abertura, largura + espaco),
+    new parDeBarreira(altura, abertura, largura + espaco * 2),
+    new parDeBarreira(altura, abertura, largura + espaco * 3),
+  ];
+
+  const deslocamento = 3;
+
+  this.animar = () => {
+    this.pares.forEach((par) => {
+      par.setX(par.getX() - deslocamento);
+
+      //quando o elemento sair da tela
+      if (par.getX() < -par.getLargura()) {
+        par.setX(par.getX() + espaco * this.pares.length);
+        par.sortearAbertura();
+      }
+
+      const meio = largura / 2;
+      const cruzouMeio = par.getX() + deslocamento >= meio && par.getX() < meio;
+      if (cruzouMeio) notificarPonto();
+    });
+  };
+}
+
+function Passaro(alturaJogo) {
+  let voando = false;
+
+  this.elemento = novoElemento("img", "passaro");
+  this.elemento.src = "imgs/passaro.png";
+
+  this.getY = () => parseInt(this.elemento.style.bottom.split("px")[0]);
+  this.setY = (y) => (this.elemento.style.bottom = `${y}px`);
+
+  window.onkeydown = (e) => (voando = true);
+  window.onkeyup = (e) => (voando = false);
+
+  this.animar = () => {
+    const novoY = this.getY() + (voando ? 8 : -5);
+    const alturaMaxima = alturaJogo - this.elemento.clientHeight;
+
+    if (novoY <= 0) {
+      this.setY(0);
+    } else if (novoY >= alturaMaxima) {
+      this.setY(alturaMaxima);
+    } else this.setY(novoY);
+  };
+
+  this.setY(alturaJogo / 2);
+}
+
+function Progresso() {
+  this.elemento = novoElemento("span", "progresso");
+  this.atualizarPontos = (pontos) => {
+    this.elemento.innerHTML = pontos;
+  };
+  this.atualizarPontos(0);
+}
+
+// const barreiras = new Barreiras(700, 1200, 200, 400);
+// const passaro = new Passaro(700);
+// const areaDoJogo = document.querySelector("[wm-flappy]");
+// areaDoJogo.appendChild(new Progresso().elemento);
+// areaDoJogo.appendChild(passaro.elemento);
+// barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.elemento));
+
+// setInterval(() => {
+//   barreiras.animar();
+//   passaro.animar();
+// }, 20);
+
+function sobrepostos(A, B) {
+  const a = A.getBoundingClientRect();
+  const b = B.getBoundingClientRect();
+
+  const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+  const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+
+  return horizontal && vertical;
+}
+
+function colidiu(passaro, barreiras) {
+  let colidiu = false;
+  barreiras.pares.forEach((parDeBarreiras) => {
+    if (!colidiu) {
+      const superior = parDeBarreiras.superior.elemento;
+      const inferior = parDeBarreiras.inferior.elemento;
+
+      colidiu =
+        sobrepostos(passaro.elemento, superior) ||
+        sobrepostos(passaro.elemento, inferior);
+    }
+  });
+  return colidiu;
+}
+
+function FlappyBird() {
+  let pontos = 0;
+
+  const areaDoJogo = document.querySelector("[wm-flappy]");
+  const altura = areaDoJogo.clientHeight;
+  const largura = areaDoJogo.clientWidth;
+
+  const progresso = new Progresso();
+  const barreiras = new Barreiras(altura, largura, 200, 400, () =>
+    progresso.atualizarPontos(++pontos)
+  );
+  const passaro = new Passaro(altura);
+
+  areaDoJogo.appendChild(progresso.elemento);
+  areaDoJogo.appendChild(passaro.elemento);
+  barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.elemento));
+
+  this.start = () => {
+    const temporizador = setInterval(() => {
+      barreiras.animar();
+      passaro.animar();
+
+      if (colidiu(passaro, barreiras)) {
+        clearInterval(temporizador);
+      }
+    }, 20);
+  };
+}
+
+new FlappyBird().start();
